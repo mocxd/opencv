091dd227ea88db584b50ee25e74a95f040b0060c
mocxd <mark.adkins.hastings@gmail.com>
2015-06-17 01:08:42 -0400

    working on front end for editor

--- js/edit.js
+++ js/edit.js
@@ -9,7 +9,21 @@ new Vue({
 });
 
 
-$(document).ready(function(){
+$(function(){
 	$('#trumbowyg-demo0').trumbowyg();
-	$('#trumbowyg-demo1').trumbowyg();
+	//$('#trumbowyg-demo1').trumbowyg();
+});
+
+$(function() {
+    $('.date-picker').datepicker( {
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+        dateFormat: 'MM yy',
+        onClose: function(dateText, inst) { 
+            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+            $(this).datepicker('setDate', new Date(year, month, 1));
+        }
+    });
 });
