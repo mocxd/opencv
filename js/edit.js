e2f790bd8bf9f57d3de91acac0f0001243bad871
mocxd <mark.adkins.hastings@gmail.com>
2015-06-18 23:26:19 -0400

    working with wys option

--- js/edit.js
+++ js/edit.js
@@ -8,6 +8,24 @@
 	}
 });*/
 
+$(function() {
+	$('.date-picker').datepicker( {
+		changeMonth: true,
+		changeYear: true,
+		showButtonPanel: true,
+		dateFormat: 'MM yy',
+		onClose: function(dateText, inst) { 
+			var month = $(".ui-datepicker-month :selected").val();
+			var year = $(".ui-datepicker-year :selected").val();
+			$(this).datepicker('setDate', new Date(year, month, 1));
+		}
+	});
+});
+
+
+$(function(){
+	$('#trumbowyg-demo0').trumbowyg();
+});
 
 
 
