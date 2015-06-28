ab41224415fdea58230243e107aebfadba3d81c0
mocxd <mark.adkins.hastings@gmail.com>
2015-06-28 03:10:27 -0400

    delete button

--- js/opencv.js
+++ js/opencv.js
@@ -19,41 +19,69 @@
             cv.push(section);
         };
 
+        this.remove = function (id) {
+            //remove a section
+
+            //find items in array by object's id
+            var indices = $.map( cv, function( elem, index ) {
+                if (elem.id === id) {
+                    return index;
+                }
+            });
+
+            //using 0th index since id should be unique
+            cv.splice(indices[0], 1);
+        };
+
         this.getCv = function () {
             return cv;
         };
     })
 
+    .service('IdService', function(){
+        var id = 0;
+
+        this.new = function () {
+            return id++;
+        };
+    })
+
     .controller('SectionController', ['$scope', '$sce', 'SectionService', function ($scope, $sce, SectionService) {
-        $scope.edit1 = false;
+        //$scope.edit1 = false;
         $scope.cv = SectionService.getCv();
-        console.log ($scope.cv);
+        //console.log ($scope.cv);
 
         $scope.saveFields = function () {
-            $scope.descriptionText0 = $sce.trustAsHtml($('#trumbowyg-demo0').val());
-            console.log ($scope.descriptionText0);
+            //$scope.descriptionText0 = $sce.trustAsHtml($('#trumbowyg-demo0').val());
+            //console.log ($scope.descriptionText0);
         };
 
         $scope.toggleState = function () {
-            $('#panel1').collapse('show');
-            $('#panel1ro').collapse('show');
-            $scope.edit1 = !$scope.edit1;
-            $scope.saveFields();
+            //$('#panel1').collapse('show');
+            //$('#panel1ro').collapse('show');
+            //$scope.edit1 = !$scope.edit1;
+            //$scope.saveFields();
         };
 
         $scope.toggleStateSingle = function() {
-            $('#panel0').collapse('show');
-            $('#panel0ro').collapse('show');
-            $scope.edit0 = !$scope.edit0;
+            //$('#panel0').collapse('show');
+            //$('#panel0ro').collapse('show');
+            //$scope.edit0 = !$scope.edit0;
+        };
+
+        $scope.remove = function() {
+            SectionService.remove($scope.sectionId);
         };
     }])
 
-    .controller('SectionPanelController', ['$scope', 'SectionService', function ($scope, SectionService){
+    .controller('SectionPanelController', ['$scope', 'SectionService', 'IdService', function ($scope, SectionService, IdService){
         $scope.newPanel = function () {
             var panel = {};
             //scope variables from directive
             panel.panelTitle = $scope.panelTitle;
-            console.log ('controller: ' + $scope.panelTitle);
+            panel.type = $scope.panelType;
+            panel.id = IdService.new();
+            //console.log ('controller: ' + $scope.panelTitle);
             SectionService.createNew(panel);
         };
     }])
@@ -69,6 +97,7 @@
             link: function(scope, elem, attrs, controller) {
                 //Load parameters from view
                 scope.panelTitle = attrs.panelTitle;
+                scope.panelType = attrs.panelType;
                 console.log ('attrs: ' + attrs.panelTitle);
                 console.log ('directive: ' + scope.panelTitle);
                 //scope.$apply();
@@ -83,7 +112,7 @@
         // name: '',
         // priority: 1,
         // terminal: true,
-        // scope: {}, // {} = isolate, true = child, false/undefined = no change
+        //scope: {},
         // controller: function($scope, $element, $attrs, $transclude) {},
         // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
         restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
@@ -92,8 +121,10 @@
         // replace: true,
         // transclude: true,
         // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-        link: function($scope, elem, attrs, controller) {
-
+        //controller: 'SectionController as sectionCtrl',
+        //bindToController: true,
+        link: function(scope, elem, attrs, controller) {
+            //scope.sectionId = attrs.sectionId;
         }
     };
 }])
