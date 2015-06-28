7a1953a62cf89359bfb4334f689c1d7b3e02e583
mocxd <mark.adkins.hastings@gmail.com>
2015-06-27 20:46:51 -0400

    fleshed out skeleton for sectionpanelcontroller/service

--- js/opencv.js
+++ js/opencv.js
@@ -1,63 +1,123 @@
 (function () {
     'use strict';
 
-angular.module('opencv', ['ngSanitize'])
+    angular.module('opencv', ['ngSanitize'])
 
-.controller('SectionController', ['$scope', '$sce', function ($scope, $sce) {
-    $scope.edit1 = false;
+    .service('SectionService', function(){
+        var cv = [];
 
-    $scope.saveFields = function () {
-        $scope.descriptionText0 = $sce.trustAsHtml($('#trumbowyg-demo0').val());
-        console.log ($scope.descriptionText0);
+        this.saveData = function () {
+            // save data to model
+        };
+
+        this.loadData = function () {
+            // load data from model
+        };
+
+        this.createNew = function (section) {
+            //create new section
+            cv.push(section);
+        };
+    })
+
+    .controller('SectionController', ['$scope', '$sce', 'SectionService', function ($scope, $sce, SectionService) {
+        $scope.edit1 = false;
+
+        $scope.saveFields = function () {
+            $scope.descriptionText0 = $sce.trustAsHtml($('#trumbowyg-demo0').val());
+            console.log ($scope.descriptionText0);
+        };
+
+        $scope.toggleState = function () {
+            $('#panel1').collapse('show');
+            $('#panel1ro').collapse('show');
+            $scope.edit1 = !$scope.edit1;
+            $scope.saveFields();
+        };
+
+        $scope.toggleStateSingle = function() {
+            $('#panel0').collapse('show');
+            $('#panel0ro').collapse('show');
+            $scope.edit0 = !$scope.edit0;
+        };
+    }])
+
+    .controller('SectionPanelController', ['$scope', 'SectionService', function ($scope, SectionService){
+        $scope.newPanel = function () {
+            var panel = {};
+                //scope variables from directive
+                panel.title = $scope.title;
+                console.log ('controller: ' + $scope.title);
+                //SectionService.createNew();
+            };
+        }])
+
+    .directive('sectionPanelBtn', ['SectionService', function (SectionService) {
+        return {
+            restrict: 'E',
+            link: function($scope, elem, attrs, controller) {
+                //Load parameters from view
+                $scope.title = attrs.panelTitle;
+                console.log ('directive: ' + $scope.title);
+                //$scope.$apply();
+            }
+        };
+    }])
+
+    .directive('section', ['SectionService', function(SectionService){
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        // scope: {}, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+         restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        // template: '',
+        // templateUrl: '',
+        // replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, elem, attrs, controller) {
+
+        }
     };
-
-    $scope.toggleState = function () {
-        $('#panel1').collapse('show');
-        $('#panel1ro').collapse('show');
-        $scope.edit1 = !$scope.edit1;
-        $scope.saveFields();
-    };
-
-    $scope.toggleStateSingle = function() {
-        $('#panel0').collapse('show');
-        $('#panel0ro').collapse('show');
-        $scope.edit0 = !$scope.edit0;
-    }
 }])
 
-.directive('wysiwyg', function() {
-    return {
-        restrict: 'E',
-        scope: false,
-        templateUrl: 'parts/wysiwyg.html',
-    };
-})
+    .directive('wysiwyg', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            templateUrl: 'parts/wysiwyg.html',
+        };
+    })
 
-.directive('wysiwygMulti', function() {
-    return {
-        restrict: 'E',
-        scope: false,
-        templateUrl: 'parts/wysiwyg-multi.html',
-    };
-})
+    .directive('wysiwygMulti', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            templateUrl: 'parts/wysiwyg-multi.html',
+        };
+    })
 
-.directive('wysiwygReadonly', function() {
-    return {
-        restrict: 'E',
-        scope: false,
-        templateUrl: 'parts/wysiwyg-readonly.html',
-    };
-})
+    .directive('wysiwygReadonly', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            templateUrl: 'parts/wysiwyg-readonly.html',
+        };
+    })
 
-.directive('wysiwygMultiReadonly', function() {
-    return {
-        restrict: 'E',
-        scope: false,
-        templateUrl: 'parts/wysiwyg-multi-readonly.html',
-    };
-})
+    .directive('wysiwygMultiReadonly', function() {
+        return {
+            restrict: 'E',
+            scope: false,
+            templateUrl: 'parts/wysiwyg-multi-readonly.html',
+        };
+    })
 
-;
+    ;
 
 
 })();
